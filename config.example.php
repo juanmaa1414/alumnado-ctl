abd7ab179791508c92de47e247d9c165dc403a54
Juan M. Fernandez <fullmedios2@gmail.com>
2017-06-01 21:15:31 -0300

    Actualizado archivo modelo de config.

--- config.example.php
+++ config.example.php
@@ -1,9 +1,13 @@
 <?php
+date_default_timezone_set('America/Argentina/Buenos_Aires');
 
 // Datos de la conexión a base de datos.
 $_db_host = "localhost";
 $_db_user = "root";
 $_db_pass = "";
 $_db_name = "alumnado";
+
+// TRUE si estamos testeando la aplicación y queremos ver errores.
+$_app_debug_mode = TRUE;
 
 // Fin del script.
